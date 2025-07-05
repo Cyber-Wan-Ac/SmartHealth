524aa281d5c764c1151fc278f285d155efdf5fa0
AzfarSyukri <muhammad.azfar.syukri@gmail.com>
2025-07-05 19:03:08 +0700

    Add files via upload

---
+++ SmartHealth/src/compunents/ui/ui/Card.jsx
@@ -0,0 +1,21 @@
+// src/components/ui/Card.jsx
+
+import React from "react";
+
+export const Card = ({ children, className = "" }) => {
+  return (
+    <div
+      className={`bg-white rounded-lg shadow-md p-4 ${className}`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const CardContent = ({ children, className = "" }) => {
+  return (
+    <div className={`text-gray-800 ${className}`}>
+      {children}
+    </div>
+  );
+};
